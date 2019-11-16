39b933ef2b466b9974013143f5b4cf2455b3ca54
Artem <golubchikov2@yandex.ru>
2019-11-16 18:02:43 +0300

    added carousel

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 var id=function(e){return document.getElementById(e);}
 
+
+function change()
+{
+	if (id("table1").style.display=="block")
+	{
+		id("table1").style.display="none";
+		id("table2").style.display="block";
+		id("table3").style.display="none";
+		return;
+	}
+	if (id("table2").style.display=="block")
+	{
+		id("table1").style.display="none";
+		id("table2").style.display="none";
+		id("table3").style.display="block";
+		return;
+	}
+	if (id("table3").style.display=="block")
+	{
+		id("table1").style.display="block";
+		id("table2").style.display="none";
+		id("table3").style.display="none";
+		return;
+	}
+	
+
+	
+}
+
+setInterval(change, 5000);
